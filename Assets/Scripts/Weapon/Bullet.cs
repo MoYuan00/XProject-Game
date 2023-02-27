a708c4197c406f4a1b721cd729e206b1cff1a415
moyuan <8692489+moyuanxx@user.noreply.gitee.com>
2023-02-27 20:43:48 +0800

    子弹碰撞后销毁，子弹特效，子弹碰撞爆炸

--- Assets/Scripts/Weapon/Bullet.cs
+++ Assets/Scripts/Weapon/Bullet.cs
@@ -18,6 +18,9 @@ public class Bullet : MonoBehaviour
     private Transform _transform;
 
     public Vector3 direction;
+
+    // 爆炸特效
+    public GameObject explodeVFX;
     
     public void Awake()
     {
@@ -28,5 +31,31 @@ public class Bullet : MonoBehaviour
     public void Start()
     {
         _rigidbody.velocity = direction * speed;
+    }
+
+    /// <summary>
+    /// 爆炸并销毁
+    /// </summary>
+    public void Explode(Vector3 position, Quaternion dir)
+    {
+        var obj = Instantiate(explodeVFX, position, dir);
+        var vfx = obj.GetComponent<ParticleSystem>();
+        vfx.gameObject.SetActive(true);
+        vfx.Play();
+        
+        Destroy(obj, vfx.main.duration);
+        Destroy(gameObject);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("CanHit"))
+        {
+            var point = collision.contacts[0];
+            var pos = point.point;
+            collision.collider.ClosestPoint(_transform.position);
+            var rotation = Quaternion.LookRotation(point.normal);
+            Explode(pos, rotation);
+        }
     }
 }
